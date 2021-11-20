c9dfafff6d8eddcb41be27396cdadaf9b225e9be
thiagowaib <thiagowaib@gmail.com>
2021-11-20 19:22:27 -0300

    [fix] Porta

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ async function initServer(){
 
     // Conexão com porta {port} + Mensagem de confirmação
     const server = require('http').Server(app);
-    server.listen(process.env.SERVER_PORT || 8080);
+    server.listen(process.env.PORT || 5000);
     
     console.log("Backend Online")
 }
